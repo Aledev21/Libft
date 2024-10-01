46adf45acc840654d1b611469c3c61928f130921
Alessandro Procopio <aledev21@gmail.com>
2024-10-01 02:20:17 -0300

    ok

---
+++ ft_strlcat.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aassis-p <aassis-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 01:27:49 by aassis-p          #+#    #+#             */
+/*   Updated: 2024/10/01 02:19:44 by aassis-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srclen;
+	size_t	dstlen;
+	size_t	i;
+
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	if (dstlen == dstsize)
+	{
+		return (dstsize + srclen);
+	}
+	i = 0;
+	while (src[i] != '\0' && (dstlen + i) < (dstsize - 1))
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	dst[dstlen + i] = '\0';
+	return (dstlen + srclen);
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aassis-p <aassis-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 00:37:57 by aassis-p          #+#    #+#             */
+/*   Updated: 2024/10/01 01:20:50 by aassis-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	len;
+
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	if (dstsize > 0)
+	{
+		i = 0;
+		while (i < dstsize -1 && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (len);
+}
