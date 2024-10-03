8554c07a3d99f4967a396fa8691c224cd853f8d5
Alessandro Assis Procopio <playcerbr@outlook.com>
2024-10-02 21:36:49 -0300

    ok

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,40 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aassis-p <aassis-p@student.42.fr>          #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-10-02 02:01:41 by aassis-p          #+#    #+#             */
-/*   Updated: 2024-10-02 02:01:41 by aassis-p         ###   ########.fr       */
+/*   Created: 2024-10-02 18:49:09 by aassis-p          #+#    #+#             */
+/*   Updated: 2024-10-02 18:49:09 by aassis-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+int	ft_atoi(const char *str)
 {
-	size_t	i;
-	size_t	j;
+	int result = 0; 
+    int sign = 1;  
+    int i = 0;       
 
-	i = 0;
-	j = 0;
-	if (little == NULL)
-	{
-		return ((char *)big);
-	}
-	while (big && i < len)
-	{
-		if (big[i] == little[j])
-		{
-			while (big[i + j] == little[j])
-			{
-				return ((char *)big + i);
-			}
-			j++;
-		}
-		i++;
-	}
-	return (NULL);
+    while (str[i] == ' ') {
+        i++;
+    }
+
+    if (str[i] == '-') {
+        sign = -1;
+        i++;
+    } else if (str[i] == '+') {
+        i++;
+    }
+
+    while (str[i] >= '0' && str[i] <= '9') {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+
+    return sign * result;
 }
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -12,22 +12,22 @@
 
 #include "libft.h"
 
-int	memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t			i;
 	unsigned char	*ss1;
 	unsigned char	*ss2;
 
-	ss1 = (unsigned char)s1;
-	ss2 = (unsigned char)s2;
+	ss1 = (unsigned char *)s1;
+	ss2 = (unsigned char *)s2;
 	i = 0;
 	while (i < n && ss1[i] != '\0' || ss2[i] != '\0')
 	{
 		i++;
 	}
-	if ((unsigned char)ss1[i] != (unsigned char)ss2[i])
+	if (ss1[i] != ss2[i])
 	{
-		return ((unsigned char)ss1[i] - (unsigned char)ss2[i]);
+		return (ss1[i] - ss2[i]);
 	}
 	else
 		return (0);
