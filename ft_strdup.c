1dd4db9b4deab6d8e85644213c67b8c4317842b5
Alessandro Procopio <aledev21@gmail.com>
2024-10-05 00:19:06 -0300

    ok

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,13 +6,42 @@
 /*   By: aassis-p <aassis-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 22:46:39 by aassis-p          #+#    #+#             */
-/*   Updated: 2024/10/04 22:47:38 by aassis-p         ###   ########.fr       */
+/*   Updated: 2024/10/04 23:18:38 by aassis-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
- char *ft_strdup(const char *s)
- {
-	
- }
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*copy;
+	size_t	size;
+	size_t	i;
+
+	i = 0;
+	size = ft_strlen(s) + 1;
+	copy = malloc(size);
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		copy[i] = s[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
